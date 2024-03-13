d281b82c383036dfe8b398f08bd07b0b976e8e54
pablo monteverde <chamyboy18@gmail.com>
2024-03-12 17:56:24 -0700

    centered game screen

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import res
 #import util
 #import random
 
-
+scale = 1.8
+ScreenW = 923
 CellCount = 13
-CellW = 1
+CellW = ScreenW / 13
 Block = 0
-BlockW = CellW *2
+BlockW = CellW * .8
+
+
 
 
 
@@ -17,13 +20,16 @@ class GameWindow(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.batch = pyglet.graphics.Batch()
-        self.square = shapes.Rectangle(160, 0, 900, 900, color=(128, 128, 128), batch=self.batch)
+        self.square = shapes.Rectangle(160, 0, 923, 923, color=(128, 128, 128), batch=self.batch)
         self.blocks = []
 
         for i in range(CellCount):
             row = []
             for j in range(CellCount):
-                row.append(object.Blocks(res.brick, BlockW * i, BlockW * j))
+                row.append(object.Blocks(res.brick, BlockW * i + 200, BlockW * j + 40))
+                row[len(row) - 1].scale = scale
+
+
                 #pyglet.sprite.Sprite()
             self.blocks.append(row)
 
@@ -34,12 +40,14 @@ class GameWindow(pyglet.window.Window):
         self.square.draw()
         for i in range (len(self.blocks)):
             for j in range(len(self.blocks[i])):
-                print(i, j)
+                print(self.blocks[i], self.blocks[j])
                 self.blocks[i][j].draw()
 
-    def update(self):
-        res.brick
-        pass
+    def update(self, dt):
+        self.square.x += 0
+
+
+
 
 
 
@@ -49,7 +57,7 @@ class GameWindow(pyglet.window.Window):
 win = GameWindow(width=1200, height=800,)
 
 
-pyglet.clock.schedule_interval(win.update() , 1.0 / 60)
+pyglet.clock.schedule_interval(win.update , 1.0 / 60)
 
 
 if __name__ ==  "__main__":
